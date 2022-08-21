c7f54dd3910ced5d5315e1926fc9c96a4923aeec
LiuQ <1639316332@qq.com>
2022-08-21 15:38:16 +0800

    git-test

---
+++ src/main/java/com/wingstudio/GitTest.java
@@ -0,0 +1,8 @@
+package com.wingstudio;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("test git");
+    }
+}
