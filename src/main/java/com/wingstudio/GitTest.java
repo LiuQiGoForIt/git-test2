8bba852a615f822a908a6b188fbc8a5d128a6eed
LiuQ <1639316332@qq.com>
2022-08-21 16:14:21 +0800

    test branches2

--- src/main/java/com/wingstudio/GitTest.java
+++ src/main/java/com/wingstudio/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("test git 2");
         System.out.println("test git 3");
         System.out.println("test branches");
+        System.out.println("test branches");
     }
 }
